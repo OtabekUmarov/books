fbb5162beef3a2192de1bebe43a60914796d7505
OtabekUmarov <otabekumarov0220@gmail.com>
2022-06-06 09:54:27 +0500

    some

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 module.exports = (req,res,next) =>{
-    // if (!req.session.isAuthed){
-    //     // res.redirect('/admin/user/login')
-    // }
+    if (!req.session.isAuthed){
+        // res.redirect('/admin/user/login')
+        next()
+    }
     next()
 }
