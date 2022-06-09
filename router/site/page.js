57423d6f1ec99ec46a332900daaa7d487f88e4f5
OtabekUmarov <otabekumarov0220@gmail.com>
2022-06-09 09:18:33 +0500

    some

--- router/site/page.js
+++ router/site/page.js
@@ -1,11 +1,11 @@
 const {Router} = require('express')
 const router = Router()
-// const Category = require('../../model/category')
+const Category = require('../../model/category')
 // const Atribut = require('../../model/atribut')
 // const Book = require('../../model/book')
 
 router.get('/',async(req,res)=>{
-    // let category = await Category.find().lean()
+    let category = await Category.find({status:1}).sort({order:1}).lean()
     // let atribut = await Atribut.find().lean()
     // let book = await Book.find().lean()
     // let bookTop = await Book.find({top:1}).lean()
@@ -13,6 +13,7 @@ router.get('/',async(req,res)=>{
         isHome:true,
         layout: 'site',
         title: 'Bosh sahifa',
+        category
     })
 })
 
